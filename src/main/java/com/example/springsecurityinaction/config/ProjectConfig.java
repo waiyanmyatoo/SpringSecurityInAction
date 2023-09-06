f9d639fcf23e9ff56436b2972a3fb53802569367
waiyanmyatoo <waiyanmyatoo@cloudsource.co.jp>
2023-09-06 11:44:33 +0630

    Extends to WebSecurityConfigAdapter and configure userDetailsS and Password Encoder

--- src/main/java/com/example/springsecurityinaction/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurityinaction/config/ProjectConfig.java
@@ -2,6 +2,9 @@ package com.example.springsecurityinaction.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,21 +13,40 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsManager = new InMemoryUserDetailsManager();
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailsManager = new InMemoryUserDetailsManager();
+//
+//        var user = User.withUsername("john").password("12345").authorities("read").build();
+//
+//        userDetailsManager.createUser(user);
+//
+//        return userDetailsManager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
+    /// alternative way to configure userdetails service and password encoder
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService = new InMemoryUserDetailsManager(); // Declares a UserDetailsSevice to store the users in memory
 
         var user = User.withUsername("john").password("12345").authorities("read").build();
 
-        userDetailsManager.createUser(user);
+        userDetailsService.createUser(user);
 
-        return userDetailsManager;
+        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
